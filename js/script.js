25aadc8b9bef135bf2e5ba099a94ce9679982137
Gabriel dos Santos <gs303291@gmail.com>
2024-07-03 18:56:35 -0300

    criação do botão CLEAR

--- js/script.js
+++ js/script.js
@@ -1,19 +1,18 @@
 const result = document.querySelector("#result");
-const operation = document.querySelector("#operation");
+const operationView = document.querySelector("#operation");
 const buttons = document.querySelectorAll("#calc .btn");
 
 class Calculator {
-  constructor(result, operation) {
+  constructor(result, operationView) {
     this.result = result;
-    this.operation = operation;
+    this.operationView = operationView;
+    this.operation = "";
     this.currentOperation = "";
   }
 
   //   Adiciona os numeros
 
   addDigit(digit) {
-    if (digit === "," && this.operation.innerHTML.includes(",")) return;
-
     this.currentOperation = digit;
     this.update();
   }
@@ -22,7 +21,7 @@ class Calculator {
 
   processOperation(operationValue) {
     console.log(this.operation.innerHTML);
-    if (this.operation.innerHTML === "") {
+    if (this.operation.innerHTML == 0) {
       return;
     }
 
@@ -35,6 +34,9 @@ class Calculator {
   toolProcess(tool) {
     switch (tool) {
       case "C":
+        this.operation = "";
+        this.currentOperation = "";
+        this.update();
         break;
       case "backspace":
         break;
@@ -42,10 +44,14 @@ class Calculator {
   }
 
   update() {
-    this.operation.innerHTML += this.currentOperation;
-    this.result.innerHTML = eval(
-      this.operation.innerHTML.replace("^", "**").replace(",", ".")
-    );
+    if (this.operation.innerHTML == 0) {
+      this.operation = this.currentOperation;
+    } else {
+      this.operation += this.currentOperation;
+    }
+    console.log(this.operation);
+    this.operationView.innerHTML = this.operation ? this.operation : 0;
+    this.result.innerHTML = eval(this.operation ? this.operation.replace("^", "**").replace(",", ".") : 0);
   }
 }
 
@@ -54,6 +60,11 @@ const calc = new Calculator(result, operation);
 buttons.forEach((btn) => {
   btn.addEventListener("click", (e) => {
     const value = e.target.innerText;
+
+    // vibrar celular
+    if ("vibrate" in navigator) {
+      navigator.vibrate(100);
+    }
 
     if (value >= 0 || value === ",") {
       calc.addDigit(value);
